c1e8321069ff958d4053e83aaf74ec199df5e56b
MateoJaramillo26 <105684625+MateoJaramillo26@users.noreply.github.com>
2024-04-10 10:38:04 -0500

    Add project files.

---
+++ MateoJaramillo_TallerMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MateoJaramillo_TallerMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
