251d1454b1738e4ee9cb52f067110d6aaf725e01
MateoJaramillo26 <105684625+MateoJaramillo26@users.noreply.github.com>
2024-04-10 10:44:29 -0500

    Creada la clase burguer

---
+++ MateoJaramillo_TallerMVC/Models/Burguer.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MateoJaramillo_TallerMVC.Models
+{
+    public class Burguer
+    { 
+        public int Id { get; set; }
+
+        [Required]
+        public String? Name { get; set; }
+        public bool? IsBurguer { get; set; }
+
+        [Range(0.01, 99999.99)]
+        public decimal? BurguerPrice { get;set; }   
+    }
+
+}
